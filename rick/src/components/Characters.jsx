6b562fca500fe4bc23a3669c3509d392f9f99373
abraham_mh <xabr12@gmail.com>
2022-11-14 13:37:22 -0600

    paginación

--- rick/src/components/Characters.jsx
+++ rick/src/components/Characters.jsx
@@ -2,27 +2,36 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from './Card';
 
+import Pagination from './Pagination';
+
 const Characters = () => {
 
     const [characters, setCharacters] = useState([]);
 
+    const [page, changePage] = useState(1);
+
+    const url = "https://rickandmortyapi.com/api/character/?page="
+
     useEffect(() => {
         axios
-            .get("https://rickandmortyapi.com/api/character")
+            .get(url + page)
             .then((resp) => {
                 setCharacters(resp.data.results);
             });
-    }, []);
+    }, [page]);
 
     return (
-        <div className='row'>
-            {characters.map((character, index) => {
-                return (
-                    <Card key={index} character={character} />
-                )
-            })}
+        <>
+            <div className='row'>
+                {characters.map((character, index) => {
+                    return (
+                        <Card key={index} character={character} />
+                    )
+                })}
 
-        </div>
+            </div>
+            <Pagination page={page} changePage={changePage} />
+        </>
     );
 }
 
---
+++ rick/src/components/Pagination.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Pagination = (props) => {
+
+    const FirstPage = () => {
+        if (props.page !== 1) {
+            return (
+                <>
+                    <li className="page-item">
+                        <button className="page-link" onClick={onClickFirst}>Primera</button>
+                    </li>
+                    <li className="page-item">
+                        <button className="page-link" onClick={onClickAnterior} >{props.page - 1}</button>
+                    </li>
+                </>
+            )
+        }
+    }
+
+    const LastPage = () => {
+        if (props.page !== 42) {
+            return (
+                <>
+                    <li className="page-item"><button className="page-link" onClick={onClickSiguiente} >{props.page + 1}</button></li>
+                    <li className="page-item">
+                        <button className="page-link" onClick={onClickLast}>Última</button>
+                    </li>
+                </>
+            )
+        }
+    }
+
+    const onClickAnterior = () => {
+        props.changePage(props.page - 1);
+    }
+
+    const onClickSiguiente = () => {
+        props.changePage(props.page + 1);
+    }
+
+    const onClickFirst = () => {
+        props.changePage(1);
+    }
+
+    const onClickLast = () => {
+        props.changePage(42);
+    }
+
+
+    return (<>
+        <nav aria-label="Page navigation example" className='bg-dark'>
+            <ul className="pagination justify-content-center ">
+                <FirstPage />
+                <li className="page-item"><button className="page-link active" >{props.page}</button></li>
+                <LastPage />
+            </ul>
+        </nav>
+    </>);
+}
+
+export default Pagination;
