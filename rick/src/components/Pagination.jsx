7437ab7c8a68660041184f49e234087d4833eaff
abraham_mh <xabr12@gmail.com>
2022-11-14 13:56:50 -0600

    muajaja

--- rick/src/components/Pagination.jsx
+++ rick/src/components/Pagination.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Pagination = (props) => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    })
 
     const FirstPage = () => {
         if (props.page !== 1) {
